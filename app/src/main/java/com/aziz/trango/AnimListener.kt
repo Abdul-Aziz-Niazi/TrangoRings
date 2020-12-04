7180d91b629b59103c6039f1446b640036d53439
Abdul-Aziz-Niazi <niazi.abdulaziz@gmail.com>
2020-12-05 00:53:19 +0500

    Half Circle Animations

---
+++ app/src/main/java/com/aziz/trango/AnimListener.kt
@@ -0,0 +1,17 @@
+package com.aziz.trango
+
+import android.view.animation.Animation
+
+abstract class AnimListener: Animation.AnimationListener {
+    override fun onAnimationStart(animation: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(animation: Animation?) {
+
+    }
+
+    override fun onAnimationRepeat(animation: Animation?) {
+
+    }
+}
--- app/src/main/java/com/aziz/trango/MainActivity.kt
+++ app/src/main/java/com/aziz/trango/MainActivity.kt
@@ -1,61 +1,37 @@
 package com.aziz.trango
 
-import android.graphics.Interpolator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.view.animation.DecelerateInterpolator
+import android.os.Handler
+import android.os.Looper
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.layers_layout.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val DELAY = 300L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val anim = createAnimation()
-        anim.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation?) {
-                Thread.sleep(150)
-                val anim2 = createAnimation()
-                shadow2.startAnimation(anim2)
-                anim2.setAnimationListener(object : Animation.AnimationListener {
-                    override fun onAnimationStart(animation: Animation?) {
-                        Thread.sleep(150)
-                        val anim3 = createAnimation()
-                        shadow3.startAnimation(anim3)
-                    }
+        Handler(Looper.getMainLooper()).postDelayed({
+            rip0.startRippleAnimation()
+            Handler(Looper.getMainLooper()).postDelayed({
+                rip1.startRippleAnimation()
+                Handler(Looper.getMainLooper()).postDelayed({
+                    rip2.startRippleAnimation()
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        rip3.startRippleAnimation()
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            rip4.startRippleAnimation()
+                            Handler(Looper.getMainLooper()).postDelayed({
+                                rip5.startRippleAnimation()
+                            }, DELAY)
+                        }, DELAY)
+                    }, DELAY)
+                }, DELAY)
+            }, DELAY)
+        }, DELAY)
 
-                    override fun onAnimationEnd(animation: Animation?) {
-
-                    }
-
-                    override fun onAnimationRepeat(animation: Animation?) {
-
-                    }
-
-                })
-            }
-
-            override fun onAnimationEnd(animation: Animation?) {
-
-            }
-
-            override fun onAnimationRepeat(animation: Animation?) {
-
-            }
-        })
-        shadow.startAnimation(anim)
-    }
-
-
-    private fun createAnimation(): Animation {
-        val anim = AnimationUtils.loadAnimation(this, android.R.anim.fade_out)
-        anim.duration = 1500
-        anim.repeatMode = Animation.REVERSE
-        anim.fillAfter = false
-        anim.repeatCount = 100
-        return anim
     }
 }
